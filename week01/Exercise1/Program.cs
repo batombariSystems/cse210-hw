05357f0cc14be01ac57d668ff1b2a55a819b8cb4
batombariSystems <bnyienakuna@byupathway.edu>
2026-07-05 13:40:21 +0100

    Finished Prep1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+       // Ask the user for their name.
+        Console.Write("what is your first name? ");
+        string first = Console.ReadLine();
+
+        Console.WriteLine("what is your last name? ");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"your name {last}, {first} {last}");
     }
 }
