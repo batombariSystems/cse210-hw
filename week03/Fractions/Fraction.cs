9bcfcd4af9253c4d60ca227b2e59e14b54fb0200
batombariSystems <bnyienakuna@byupathway.edu>
2026-07-17 00:50:54 +0100

    Program Journal Ex

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,60 @@
+public class Fraction
+{
+    // Private attributes
+    private int _top;
+    private int _bottom;
+
+    // Constructor: 1/1
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    // Constructor: whole number/1
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    // Constructor: top/bottom
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    // Getters and Setters
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+
+    // Returns fraction as a string
+    public string GetFractionString()
+    {
+        return $"{_top}/{_bottom}";
+    }
+
+    // Returns decimal value
+    public double GetDecimalValue()
+    {
+        return (double)_top / _bottom;
+    }
+}
--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+
+    {
+        // Test first constructor
+        Fraction f1 = new Fraction();
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        // Test second constructor
+        Fraction f2 = new Fraction(5);
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        // Test third constructor
+        Fraction f3 = new Fraction(3, 4);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        Fraction f4 = new Fraction(1, 3);
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
+
+        // Test getters and setters
+        Fraction f5 = new Fraction();
+        f5.SetTop(6);
+        f5.SetBottom(7);
+
+        Console.WriteLine($"Top: {f5.GetTop()}");
+        Console.WriteLine($"Bottom: {f5.GetBottom()}");
+        Console.WriteLine(f5.GetFractionString());
+        Console.WriteLine(f5.GetDecimalValue());
     }
 }
+
+    }
